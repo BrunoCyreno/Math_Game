a16d9ee0932c01fe87e57f5ec50f774f899fe2ca
Bruno Cyreno da Silva <114535946+BrunoCyreno@users.noreply.github.com>
2024-02-12 22:04:16 -0300

    feat: add addition game functions

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,50 +1,97 @@
-﻿Console.WriteLine("Player Name: ");
+﻿var date = DateTime.UtcNow;
 
-var name = Console.ReadLine();
-var date = DateTime.UtcNow;
+string name = getName();
 
-//Game main menu. Prompt to select game mode.
-Console.WriteLine("*****************************************************");
-Console.WriteLine($"Hello {name.ToUpper()}. Hope you're having a good {date.DayOfWeek}. This is a simple math game.\n");
-Console.WriteLine(@$"Select your game mode:
+Menu(name);
+
+string getName()
+{
+    Console.WriteLine("Player Name: ");
+    var name = Console.ReadLine();
+    return name;
+}
+void Menu(string name)
+{
+    //Game main menu. Prompt to select game mode.
+    Console.WriteLine("*****************************************************");
+    Console.WriteLine($"Hello {name.ToUpper()}. Hope you're having a good {date.DayOfWeek}. This is a simple math game.\n");
+    Console.WriteLine(@$"Select your game mode:
 A - Addition
 S - Subtraction
 M - Multiplication
 D - Division
 Q - Quit Game");
-Console.WriteLine("*****************************************************");
+    Console.WriteLine("*****************************************************");
 
-//Save user input and start selected game mode
-var gameSelected = Console.ReadLine().Trim().ToUpper();
+    //Save user input and start selected game mode
+    var gameSelected = Console.ReadLine().Trim().ToUpper();
 
-switch (gameSelected)
-{
-    case "A":
-        Console.WriteLine("Game Mode: Addition");
-        break;
+    switch (gameSelected)
+    {
+        case "A":
+            Console.WriteLine("Game Mode: Addition");
+            additionGame();
+            break;
 
-    case "S":
-        Console.WriteLine("Game Mode: Subtraction");
-        break;
-    case "M":
-        Console.WriteLine("Game Mode: Multiplication");
-        break;
-    case "D":
-        Console.WriteLine("Game Mode: Division");
-        break;
-    case "Q":
-        Console.WriteLine("Are you sure that you want to quit the game? [Y/N]");
+        case "S":
+            Console.WriteLine("Game Mode: Subtraction");
+            subtractionGame();
+            break;
 
-        var quitOption = Console.ReadLine();
+        case "M":
+            Console.WriteLine("Game Mode: Multiplication");
+            multiplicationGame();
+            break;
 
-        if (quitOption.Trim().ToUpper() == "Y")
-        {
+        case "D":
+            Console.WriteLine("Game Mode: Division");
+            divisionGame();
+            break;
+
+        case "Q":
+            Console.WriteLine("Are you sure that you want to quit the game? [Y/N]");
+
+            var quitOption = Console.ReadLine();
+
+            if (quitOption.Trim().ToUpper() == "Y")
+            {
+                Environment.Exit(0);
+            }
+            break;
+
+        default:
+            Console.WriteLine("Invalid Input");
             Environment.Exit(0);
+            break;
+    }
+}
+//Create methods for each game modes.
+void additionGame()
+{
+    int lives = 3;
+    var random = new Random();
+    while (lives > 0)
+    {
+        int firstNumber = random.Next(0, 10);
+        int secondNumber = random.Next(0, 10);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var answer = Console.ReadLine();
+
+        if (int.Parse(answer) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Correct!");
         }
-        break;
-
-
-
-
+        else
+        {
+            Console.WriteLine("Wrong!");
+            lives--;
+        }
+    }
 
 }
+void subtractionGame() { }
+void multiplicationGame() { }
+void divisionGame() { }
+
+
